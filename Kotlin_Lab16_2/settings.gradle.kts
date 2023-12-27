ea797eccd899e3adc2de5571504a7e5b88c70ba0
tyler0501 <tyler920501@gmail.com>
2023-12-27 14:59:22 +0800

    HW6

---
+++ Kotlin_Lab16_2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin_Lab16_2"
+include(":app")
+ 
